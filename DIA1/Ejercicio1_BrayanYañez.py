f47ad2124a0a02973d73a1170136bd9523ddfea9
Brayan <brayanbetancur2610@gmail.com>
2024-04-09 12:56:54 -0500

    phyton

---
+++ DIA1/Ejercicio1_BrayanYañez.py
@@ -0,0 +1,47 @@
+#--------------------------------
+#---- DIA 1 CHEAT SHEET PYTHON ------
+#--------------------------------
+
+#Imprimir hola mundo
+print("Hola mundoooooooooooooo!!!")
+
+#Datos primitivos
+
+#Numero
+numerito = 1 #nombreVariable = valor
+print(numerito) #imprimir(variable)
+print(type(numerito)) #imprimir(tipo(variable))
+
+#Decimal
+numeritoDecimal = 1.1
+print(numeritoDecimal)
+print(type(numeritoDecimal))
+
+#Booleano
+miBooleanito = True
+print(miBooleanito)
+print(type(miBooleanito))
+
+#Cadena de texto
+texto = "MI TIBU"
+print(texto)
+print(type(texto))
+
+#TRANAJO PEDIDO 
+
+#Ingresa por teclado la infomarcion
+#--------------------------------------
+print ("")
+print ("ingresa tu nombre")
+nombre = input ()
+print ("bienvenido", nombre, ".gracias por usar el programa",sep="")
+#--------------------------------------
+
+#Conversion de tipos de variable
+
+#Bucles For y While
+
+#Funciones (4 Tipos)
+
+#Desarrollado por Brayan Yañez c.c 1093925543
+
